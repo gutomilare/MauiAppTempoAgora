9702ad9e3995706895629c2f6167169bf5c05a56
gutomilare <gutomilare@gmail.com>
2025-04-06 15:48:25 -0300

    Criação do APP  - Infos por cidade:   > Latitude   > Longitude   > Nascer do Sol   > Por do Sol   > Temp Máx   > Temp Mín

---
+++ MauiAppTempoAgora/Models/Tempo.cs
@@ -0,0 +1,27 @@
+﻿namespace MauiAppTempoAgora.Models
+{
+    public class Tempo
+    {
+        // Latitude e Longitude
+        public double? lon { get; set; }
+        public double? lat { get; set; }
+
+        // Temperatura Mínima e Máxima
+        public double? temp_min { get; set; }
+        public double? temp_max { get; set; }
+
+        // Visibilidade
+        public int? visibility { get; set; }
+
+        // Nascer e Por do Sol
+        public string? sunrise { get; set; }
+        public string? sunset { get; set; }
+
+        // Descrições
+        public string? main { get; set; }
+        public string? description { get; set; }
+
+        // Vento
+        public double? speed { get; set; }
+    }
+}
